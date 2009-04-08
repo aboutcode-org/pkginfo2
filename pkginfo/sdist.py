9ba5da21a034b2bfaf0987492cd1f440f8455209
Tres Seaver <tseaver@agendaless.com>
2009-04-08 19:23:49 -0400

    Get initial doctest running.
    original bzr status:
    revno: 2
    committer: Tres Seaver <tseaver@agendaless.com>
    branch nick: pkginfo
    timestamp: Wed 2009-04-08 19:23:49 -0400

---
+++ pkginfo/sdist.py
@@ -0,0 +1,9 @@
+class SDist:
+    metadata_version = None
+    name = None
+    version = None
+    home_page = None
+    classifiers = ()
+    supported_platforms = ()
+    def __init__(self, filename):
+        pass
--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import os
 try:
     from setuptools import setup
+    extras = {
+        'tests_require': ['zope.testing'],
+        'test_suite': 'pkginfo.tests',
+    }
 except ImportError:
     from distutils import setup
+    extras = {}
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
@@ -12,4 +17,5 @@ setup(
     name='pkginfo',
     version='0.1',
     description='Query PKG-INFO data from sdist archives.',
+    **extras
 )
