b9951770d05ddc730f763eb62a82be95f41f64f3
Tres Seaver <tseaver@agendaless.com>
2009-05-07 11:32:59 -0400

    Speling.

--- pkginfo/tests/test_bdist.py
+++ pkginfo/tests/test_bdist.py
@@ -1,6 +1,6 @@
 import unittest
 
-class SDistTests(unittest.TestCase):
+class BDistTests(unittest.TestCase):
 
     def _getTargetClass(self):
         from pkginfo.bdist import BDist
@@ -9,21 +9,21 @@ class SDistTests(unittest.TestCase):
     def _makeOne(self, filename=None):
         return self._getTargetClass()(filename)
 
-    def _checkSample(self, sdist, filename):
-        self.assertEqual(sdist.filename, filename)
-        self.assertEqual(sdist.metadata_version, '1.0')
-        self.assertEqual(sdist.name, 'mypackage')
-        self.assertEqual(sdist.version, '0.1')
-        self.assertEqual(sdist.keywords, None)
-        self.assertEqual(list(sdist.classifiers),
+    def _checkSample(self, bdist, filename):
+        self.assertEqual(bdist.filename, filename)
+        self.assertEqual(bdist.metadata_version, '1.0')
+        self.assertEqual(bdist.name, 'mypackage')
+        self.assertEqual(bdist.version, '0.1')
+        self.assertEqual(bdist.keywords, None)
+        self.assertEqual(list(bdist.classifiers),
                          ['Development Status :: 4 - Beta',
                           'Environment :: Console (Text Based)',
                          ])
-        self.assertEqual(list(sdist.supported_platforms), [])
+        self.assertEqual(list(bdist.supported_platforms), [])
 
     def test_ctor_w_egg(self):
         import os
         d, _ = os.path.split(__file__)
         filename = '%s/../../docs/examples/mypackage-0.1-py2.6.egg' % d
-        sdist = self._makeOne(filename)
-        self._checkSample(sdist, filename)
+        bdist = self._makeOne(filename)
+        self._checkSample(bdist, filename)
