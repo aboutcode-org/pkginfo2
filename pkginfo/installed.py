c1c9139f4b560f328f63b92ded2d10e92a8177b6
Tres Seaver <tseaver@agendaless.com>
2009-05-07 19:13:41 -0400

    Fixed bugs handling installed packages which lack ``__file__`` or ``PKG-INFO``.

--- pkginfo/installed.py
+++ pkginfo/installed.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import sys
+import warnings
 
 from pkginfo.distribution import Distribution
 
@@ -8,23 +9,33 @@ class Installed(Distribution):
 
     def __init__(self, package, metadata_version=None):
         if isinstance(package, basestring):
-            __import__(package)
-            package = sys.modules[package]
+            self.package_name = package
+            try:
+                __import__(package)
+            except ImportError:
+                package = None
+            else:
+                package = sys.modules[package]
+        else:
+            self.package_name = package.__name__
         self.package = package
         self.metadata_version = metadata_version
         self.extractMetadata()
 
     def read(self):
-        package = self.package.__package__
-        pattern = '%s*.egg-info' % package
-        dir, name = os.path.split(self.package.__file__)
-        candidates = glob.glob(os.path.join(dir, pattern))
-        candidates.extend(glob.glob(os.path.join(dir, '..', pattern)))
-        for candidate in candidates:
-            if os.path.isdir(candidate):
-                path = os.path.join(candidate, 'PKG-INFO')
-            else:
-                path = candidate
-            if os.path.exists(path):
-                return open(path).read()
-        warnings.warn('No PKG-INFO found for package: %s' % package)
+        if self.package is not None:
+            package = self.package.__package__
+            pattern = '%s*.egg-info' % package
+            file = getattr(self.package, '__file__', None)
+            if file is not None:
+                dir, name = os.path.split(self.package.__file__)
+                candidates = glob.glob(os.path.join(dir, pattern))
+                candidates.extend(glob.glob(os.path.join(dir, '..', pattern)))
+                for candidate in candidates:
+                    if os.path.isdir(candidate):
+                        path = os.path.join(candidate, 'PKG-INFO')
+                    else:
+                        path = candidate
+                    if os.path.exists(path):
+                        return open(path).read()
+        warnings.warn('No PKG-INFO found for package: %s' % self.package_name)
--- pkginfo/tests/test_installed.py
+++ pkginfo/tests/test_installed.py
@@ -11,13 +11,41 @@ class InstalledTests(unittest.TestCase):
             return self._getTargetClass()(filename, metadata_version)
         return self._getTargetClass()(filename)
 
+    def test_ctor_w_package_no___file__(self):
+        import sys
+        import warnings
+        old_filters = warnings.filters[:]
+        warnings.filterwarnings('ignore')
+        try:
+            installed = self._makeOne(sys)
+            self.assertEqual(installed.package, sys)
+            self.assertEqual(installed.package_name, 'sys')
+            self.assertEqual(installed.metadata_version, None)
+        finally:
+            warnings.filters[:] = old_filters
+
     def test_ctor_w_package(self):
         import pkginfo
         from pkginfo.tests import _checkSample
         installed = self._makeOne(pkginfo)
+        self.assertEqual(installed.package, pkginfo)
+        self.assertEqual(installed.package_name, 'pkginfo')
         self.assertEqual(installed.metadata_version, '1.0')
-        self.assertEqual(installed.package.__name__, 'pkginfo')
         _checkSample(self, installed)
+
+    def test_ctor_w_package_no_PKG_INFO(self):
+        import types
+        import warnings
+        from pkginfo.tests import _checkSample
+        old_filters = warnings.filters[:]
+        warnings.filterwarnings('ignore')
+        try:
+            installed = self._makeOne(types)
+            self.assertEqual(installed.package, types)
+            self.assertEqual(installed.package_name, 'types')
+            self.assertEqual(installed.metadata_version, None)
+        finally:
+            warnings.filters[:] = old_filters
 
     def test_ctor_w_package_and_metadata_version(self):
         import pkginfo
@@ -28,15 +56,26 @@ class InstalledTests(unittest.TestCase):
         _checkSample(self, installed)
 
     def test_ctor_w_name(self):
+        import pkginfo
         from pkginfo.tests import _checkSample
         installed = self._makeOne('pkginfo')
         self.assertEqual(installed.metadata_version, '1.0')
-        self.assertEqual(installed.package.__name__, 'pkginfo')
+        self.assertEqual(installed.package, pkginfo)
+        self.assertEqual(installed.package_name, 'pkginfo')
         _checkSample(self, installed)
 
     def test_ctor_w_name_and_metadata_version(self):
+        import pkginfo
         from pkginfo.tests import _checkSample
         installed = self._makeOne('pkginfo', metadata_version='1.2')
         self.assertEqual(installed.metadata_version, '1.2')
-        self.assertEqual(installed.package.__name__, 'pkginfo')
+        self.assertEqual(installed.package, pkginfo)
+        self.assertEqual(installed.package_name, 'pkginfo')
         _checkSample(self, installed)
+
+    def test_ctor_w_invalid_name(self):
+        installed = self._makeOne('nonesuch')
+        self.assertEqual(installed.package, None)
+        self.assertEqual(installed.package_name, 'nonesuch')
+        self.assertEqual(installed.metadata_version, None)
+
