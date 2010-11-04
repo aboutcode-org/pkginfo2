3bd42e3390f6649fddec8594500bc12a20321fb7
Tres Seaver <tseaver@agendaless.com>
2010-11-04 14:15:30 -0400

    100% coverage for utils.

--- pkginfo/tests/test_utils.py
+++ pkginfo/tests/test_utils.py
@@ -126,3 +126,36 @@ class Test_get_metadata(unittest.TestCase):
             self.assertEqual(dist.metadata_version, '1.2')
             _checkSample(self, dist)
             _checkClassifiers(self, dist)
+
+    def test_w_directory_no_EGG_INFO(self):
+        import os
+        import warnings
+        dir, name = os.path.split(__file__)
+        subdir = os.path.join(dir, 'funny')
+        old_filters = warnings.filters[:]
+        warnings.filterwarnings('ignore')
+        try:
+            dist = self._callFUT(subdir)
+            self.assertEqual(dist.path, subdir)
+            self.assertEqual(dist.name, None)
+            self.assertEqual(dist.version, None)
+        finally:
+            warnings.filters[:] = old_filters
+
+    def test_w_directory(self):
+        import os
+        dir, name = os.path.split(__file__)
+        subdir = os.path.join(dir, 'silly')
+        dist = self._callFUT(subdir)
+        self.assertEqual(dist.metadata_version, '1.0')
+        self.assertEqual(dist.name, 'silly')
+        self.assertEqual(dist.version, '0.1')
+
+    def test_w_directory_and_metadata_version(self):
+        import os
+        dir, name = os.path.split(__file__)
+        subdir = os.path.join(dir, 'silly')
+        dist = self._callFUT(subdir, metadata_version='1.2')
+        self.assertEqual(dist.metadata_version, '1.2')
+        self.assertEqual(dist.name, 'silly')
+        self.assertEqual(dist.version, '0.1')
