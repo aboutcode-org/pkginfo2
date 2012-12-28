7ad30abe2e233e2af76e0dd13d8c234752b48dfe
Tres Seaver <tseaver@agendaless.com>
2012-12-28 15:33:54 -0500

    Add support for Python 3.2
    Test it under ``tox``.

--- pkginfo/tests/__init__.py
+++ pkginfo/tests/__init__.py
@@ -22,6 +22,7 @@ def _checkClassifiers(testcase, installed):
       'Operating System :: OS Independent',
       'Programming Language :: Python :: 2.6',
       'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3.2',
       'Programming Language :: Python :: Implementation :: CPython',
       'Programming Language :: Python :: Implementation :: PyPy',
       'Topic :: Software Development :: Libraries :: Python Modules',
