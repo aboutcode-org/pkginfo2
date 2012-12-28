7b121a5e6cab4a05ad19ff2a45df3464ff7e2ee4
Tres Seaver <tseaver@agendaless.com>
2012-12-28 15:33:54 -0500

    Add support for Python 3.2
    Test it under ``tox``.
    
    original bzr status:
    revno: 102
    committer: Tres Seaver <tseaver@agendaless.com>
    branch nick: pkginfo
    timestamp: Fri 2012-12-28 15:33:54 -0500

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
       'Operating System :: OS Independent',
       'Programming Language :: Python :: 2.6',
       'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3.2',
       'Programming Language :: Python :: Implementation :: CPython',
       'Programming Language :: Python :: Implementation :: PyPy',
       'Topic :: Software Development :: Libraries :: Python Modules',
