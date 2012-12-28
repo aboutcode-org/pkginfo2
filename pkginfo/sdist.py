a8fb37657402ec1b175d9276e067af8ce064ea51
Tres Seaver <tseaver@agendaless.com>
2012-12-28 15:21:05 -0500

    Clarify bytes usage.
    original bzr status:
    revno: 96
    committer: Tres Seaver <tseaver@agendaless.com>
    branch nick: pkginfo
    timestamp: Fri 2012-12-28 15:21:05 -0500

--- pkginfo/sdist.py
+++ pkginfo/sdist.py
@@ -35,7 +35,7 @@ class SDist(Distribution):
         for path in [x[1] for x in schwarz]:
             candidate = '/'.join(path)
             data = read_file(candidate)
-            if 'Metadata-Version' in data:
+            if b'Metadata-Version' in data:
                 return data
 
         raise ValueError('No PKG-INFO in archive: %s' % fqn)
