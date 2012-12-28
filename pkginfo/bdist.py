fb4ccb08962266785b11875b2ae7d07f52ac1c19
Tres Seaver <tseaver@agendaless.com>
2012-12-28 17:07:03 -0500

    Suppress resource warning leaks reported against clients under Py3k.
    original bzr status:
    revno: 109
    committer: Tres Seaver <tseaver@agendaless.com>
    branch nick: pkginfo
    timestamp: Fri 2012-12-28 17:07:03 -0500

--- pkginfo/bdist.py
+++ pkginfo/bdist.py
@@ -24,13 +24,16 @@ class BDist(Distribution):
         else:
             raise ValueError('Not a known archive format: %s' % fqn)
 
-        tuples = [x.split('/') for x in names if 'PKG-INFO' in x]
-        schwarz = sorted([(len(x), x) for x in tuples])
-        for path in [x[1] for x in schwarz]:
-            candidate = '/'.join(path)
-            data = read_file(candidate)
-            if b'Metadata-Version' in data:
-                return data
+        try:
+            tuples = [x.split('/') for x in names if 'PKG-INFO' in x]
+            schwarz = sorted([(len(x), x) for x in tuples])
+            for path in [x[1] for x in schwarz]:
+                candidate = '/'.join(path)
+                data = read_file(candidate)
+                if b'Metadata-Version' in data:
+                    return data
+        finally:
+            archive.close()
 
         raise ValueError('No PKG-INFO in archive: %s' % fqn)
 
