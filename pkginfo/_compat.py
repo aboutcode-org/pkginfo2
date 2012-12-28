cb7eeb35d1d9c3cd6ab71ea17d31590e85cf8c45
Tres Seaver <tseaver@agendaless.com>
2012-12-28 15:48:40 -0500

    Mark of coverage.
    original bzr status:
    revno: 104
    committer: Tres Seaver <tseaver@agendaless.com>
    branch nick: pkginfo
    timestamp: Fri 2012-12-28 15:48:40 -0500

--- pkginfo/_compat.py
+++ pkginfo/_compat.py
@@ -1,26 +1,26 @@
 try:
     STRING_TYPES = (str, unicode)
-except NameError: #pragma NO COVER Python >= 3.0
+except NameError:           #pragma NO COVER Python >= 3.0
     STRING_TYPES = (str,)
 
 try:
     u = unicode
-except NameError: #pragma NO COVER Python >= 3.0
+except NameError:           #pragma NO COVER Python >= 3.0
     u = str
     b = bytes
-else: #pragma NO COVER Python < 3.0
+else:                       #pragma NO COVER Python < 3.0
     b = str
 
 try:
     from StringIO import StringIO
-except ImportError: #pragma NO COVER Python >= 3.0
+except ImportError:         #pragma NO COVER Python >= 3.0
     from io import StringIO
     from io import BytesIO
-else:
+else:                       #pragma NO COVER Python < 3.0
     BytesIO = StringIO
 
 
-def must_decode(value):
+def must_decode(value):     #pragma NO COVER
     if type(value) is bytes:
         try:
             return value.decode('utf-8')
@@ -28,7 +28,7 @@ def must_decode(value):
             return value.decode('latin1')
     return value
 
-def must_encode(value):
+def must_encode(value):     #pragma NO COVER
     if type(value) is u:
         return value.encode('utf-8')
     return value
