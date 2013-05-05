997e91f69a23f40b8ff6f0b04686ee36c5f76f72
Tres Seaver <tseaver@agendaless.com>
2013-05-05 11:52:03 -0400

    Support "develop" packages which keep their '*.egg-info' in a subdirectory.
    See https://bugs.launchpad.net/pkginfo/+bug/919147.

--- pkginfo/develop.py
+++ pkginfo/develop.py
@@ -1,8 +1,29 @@
-import glob
 import os
+import sys
 import warnings
 
 from pkginfo.distribution import Distribution
+
+def _gather_py2(top, candidates): #pragma NO COVER Py3k
+    def _filter(candidates, dirname, fnames):
+        for fname in fnames:
+            fqn = os.path.join(dirname, fname)
+            if os.path.isdir(fqn):
+                if fname == 'EGG-INFO' or fname.endswith('.egg-info'):
+                    candidates.append(fqn)
+    os.path.walk(top, _filter, candidates)
+
+def _gather_py3(top, candidates): #pragma NO COVER Python2
+    for dirpath, dirnames, fnames in os.walk(top):
+        for dirname in dirnames:
+            fqn = os.path.join(dirpath, dirname)
+            if dirname == 'EGG-INFO' or dirname.endswith('.egg-info'):
+                candidates.append(fqn)
+
+if sys.version_info[0] < 3: #pragma NO COVER Python2
+    _gather = _gather_py2
+else: #pragma NO COVER Py3k
+    _gather = _gather_py3
 
 class Develop(Distribution):
 
@@ -15,12 +36,10 @@ class Develop(Distribution):
 
     def read(self):
         candidates = [self.path]
-        candidates.extend(glob.glob(os.path.join(self.path, '*.egg-info')))
-        candidates.extend(glob.glob(os.path.join(self.path, 'EGG-INFO')))
+        _gather(self.path, candidates)
         for candidate in candidates:
-            if os.path.isdir(candidate):
-                path = os.path.join(candidate, 'PKG-INFO')
-                if os.path.exists(path):
-                    with open(path) as f:
-                        return f.read()
+            path = os.path.join(candidate, 'PKG-INFO')
+            if os.path.exists(path):
+                with open(path) as f:
+                    return f.read()
         warnings.warn('No PKG-INFO found for path: %s' % self.path)
