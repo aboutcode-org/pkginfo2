c51fe0501a2619b8b57125c6b3df8a900059764d
Tres Seaver <tseaver@agendaless.com>
2015-01-02 12:56:35 -0500

    ssage: Add 100% test coverage for 'pkginfo.commandline'.
    Fix Py3k incompatibilities thereby revealed.
    
    original bzr status:
    revno: 127
    fixes bug: https://launchpad.net/bugs/1367434
    committer: Tres Seaver <tseaver@agendaless.com>
    branch nick: pkginfo

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 setup(
     name='pkginfo',
-    version='1.2b1',
+    version='1.2.dev0',
     description='Query metadatdata from sdists / bdists / installed packages.',
     platforms=['Unix', 'Windows'],
     long_description='\n\n'.join([README, CHANGES]),
